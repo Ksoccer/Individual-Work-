5e716f02bdaf1460300aa37a755b3653ca112420
Ksoccer <65356275+Ksoccer@users.noreply.github.com>
2020-05-18 12:02:54 -0500

    Update Run_cunit.c
    Commented out all code I did not write

--- Run_cunit.c
+++ Run_cunit.c
@@ -1,7 +1,7 @@
 #include <CUnit/CUnit.h>
 #include <CUnit/Basic.h>
 #include "Faults.h"
-
+/*
 void test_UnderVoltage(void)
 {
     CU_ASSERT(UnderVoltage(4.0,20) == 1);
@@ -14,13 +14,13 @@ void test_UnderFrequency(void)
     CU_ASSERT(UnderFrequency(350.0,20) == 0);
     CU_ASSERT(UnderFrequency(340.0,19) == 0);
 }
-/*
+*/
 void test_OverCurrent(void)
 {
     CU_ASSERT(OverCurrent(11,20) == 1);
     CU_ASSERT(OverCurrent(10,20) == 0);
     CU_ASSERT(OverCurrent(11,19) == 0);
-}*/
+}
 
 /*
 void test_OverVoltage(void)
@@ -33,6 +33,7 @@ void test_OverVoltage(void)
 
 int main()
 {
+	/*
     CU_initialize_registry();
     CU_pSuite suite = CU_add_suite("test_UnderVoltage", 0, 0);
 
@@ -41,8 +42,8 @@ int main()
 	CU_pSuite suite2 = CU_add_suite("test_UnderFrequency", 0, 0);
 
     CU_add_test(suite2, "test_UnderFrequency", test_UnderFrequency);
-	
-	/*CU_pSuite suite3 = CU_add_suite("test_OverCurrent", 0, 0);
+	*/
+	CU_pSuite suite3 = CU_add_suite("test_OverCurrent", 0, 0);
 
     CU_add_test(suite3, "test_OverCurrent", test_OverCurrent);*/
 	
