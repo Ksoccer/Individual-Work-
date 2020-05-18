9b7337209786012760ad8113fd1928305f2f0fcf
Ksoccer <65356275+Ksoccer@users.noreply.github.com>
2020-05-18 12:07:56 -0500

    Update Over_Current.c

--- Over_Current.c
+++ Over_Current.c
@@ -1,29 +1,3 @@
-/* #include <stdio.h>
-#include "Faults.h"
-
-bool UnderCurrent(int PHA_I,int PHB_I,int PHC_I, int time)
-{
-   int PHUI;
-   int flag_undercurrent;
- 
-    if (PHA_I >= PHB_I) {
-        PHUI = PHA_I;
-    } else {
-        PHUI = PHB_I;
-    }
-    if (PHUI >= PHC_I) {
-    } else {
-       PHUI = PHC_I;
-    }
- 
- if(PHUI<K_UI_MIN && time==20)
- {
-   return = 1; 
- } 
- else()
-   return = 0;
-}
-*/
 #include <stdio.h>
 #include "Faults.h"
 #include <stdbool.h>
